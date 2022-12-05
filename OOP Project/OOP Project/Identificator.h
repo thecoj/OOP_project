1575ac922eabb9193a00f151d983a30098e336c1
Cojocarita Cristian <cojocaritacristian21@stud.ase.ro>
2022-12-05 15:47:34 +0200

    Phase 1 completed

---
+++ OOP Project/OOP Project/Identificator.h
@@ -0,0 +1,13 @@
+enum Token
+{
+	IDENTIFICATOR = 128,
+	INTEGER_NB,
+	FLOAT_NB,
+	STRING,
+
+	ALL, CREATE, DELETE, DISPLAY, DROP, EXISTS, FLOAT, FROM, IMPORT, INDEX, INSERT, INT, INTO, NOT, ON, SELECT, SET, CSV, CHAR, TABLE, UPDATE, VALUES, WHERE
+};
+
+char* readCommand(char* in);
+//char* commandLine(char* input);
+void commandType(char* command, int* tokenType);
